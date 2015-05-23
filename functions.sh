2071dcfb811d2931f286f7424d55f639afd0cf08
blindpet <blindpet@gmail.com>
2015-05-23 11:33:25 +0200

    Update functions.sh
    new sonarr init.d

--- functions.sh
+++ functions.sh
@@ -264,126 +264,111 @@ debconf-apt-progress -- apt-get install nzbdrone -y
 sudo chown -R $NZBDRONEUSER:$NZBDRONEUSER /opt/NzbDrone
 #Create nzbdrone script
 cat > /etc/init.d/nzbdrone <<EOT
-#! /bin/sh
+#!/bin/bash
+ 
 ### BEGIN INIT INFO
-# Provides: NzbDrone
-# Required-Start: $local_fs $network $remote_fs
-# Required-Stop: $local_fs $network $remote_fs
-# Should-Start: $NetworkManager
-# Should-Stop: $NetworkManager
-# Default-Start: 2 3 4 5
-# Default-Stop: 0 1 6
-# Short-Description: starts instance of NzbDrone
-# Description: starts instance of NzbDrone using start-stop-daemon
+# Provides:          nzbdrone
+# Required-Start:    $local_fs $network $remote_fs
+# Required-Stop:     $local_fs $network $remote_fs
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: nzbdrone
+# Description:       NZBDrone
 ### END INIT INFO
-
-############### EDIT ME ##################
-# path to app
-APP_PATH=/opt/NzbDrone
-
-# user
-RUN_AS=$NZBDRONEUSER
-
-# path to mono bin
-DAEMON=/usr/local/bin/mono
-
-# Path to store PID file
-PID_FILE=/var/run/nzbdrone/nzbdrone.pid
-PID_PATH=$(dirname $PID_FILE)
-
-# script name
-NAME=nzbdrone
-
-# app name
-DESC=NzbDrone
-
-# startup args
-EXENAME="NzbDrone.exe"
-DAEMON_OPTS=" "$EXENAME
-
-############### END EDIT ME ##################
-
-NZBDRONE_PID=`ps auxf | grep NzbDrone.exe | grep -v grep | awk '{print $2}'`
-
-test -x $DAEMON || exit 0
-
-set -e
-
-#Look for PID and create if doesn't exist
-if [ ! -d $PID_PATH ]; then
-mkdir -p $PID_PATH
-chown $RUN_AS $PID_PATH
-fi
-
-if [ ! -d $DATA_DIR ]; then
-mkdir -p $DATA_DIR
-chown $RUN_AS $DATA_DIR
-fi
-
-if [ -e $PID_FILE ]; then
-PID=`cat $PID_FILE`
-if ! kill -0 $PID > /dev/null 2>&1; then
-echo "Removing stale $PID_FILE"
-rm $PID_FILE
-fi
-fi
-
-echo $NZBDRONE_PID > $PID_FILE
-
-case "$1" in
-start)
-if [ -z "${NZBDRONE_PID}" ]; then
-echo "Starting $DESC"
-rm -rf $PID_PATH || return 1
-install -d --mode=0755 -o $RUN_AS $PID_PATH || return 1
-start-stop-daemon -d $APP_PATH -c $RUN_AS --start --background --pidfile $PID_FILE --exec $DAEMON -- $DAEMON_OPTS
-else
-echo "NzbDrone already running."
-fi
-;;
-stop)
-echo "Stopping $DESC"
-echo $NZBDRONE_PID > $PID_FILE
-start-stop-daemon --stop --pidfile $PID_FILE --retry 15
-;;
-
-restart|force-reload)
-echo "Restarting $DESC"
-start-stop-daemon --stop --pidfile $PID_FILE --retry 15
-start-stop-daemon -d $APP_PATH -c $RUN_AS --start --background --pidfile $PID_FILE --exec $DAEMON -- $DAEMON_OPTS
-;;
-status)
-# Use LSB function library if it exists
-if [ -f /lib/lsb/init-functions ]; then
+ 
 . /lib/lsb/init-functions
-if [ -e $PID_FILE ]; then
-status_of_proc -p $PID_FILE "$DAEMON" "$NAME" && exit 0 || exit $?
-else
-log_daemon_msg "$NAME is not running"
-exit 3
-fi
-
-else
-# Use basic functions
-if [ -e $PID_FILE ]; then
-PID=`cat $PID_FILE`
-if kill -0 $PID > /dev/null 2>&1; then
-echo " * $NAME is running"
-exit 0
-fi
-else
-echo " * $NAME is not running"
-exit 3
-fi
-fi
-;;
-*)
-N=/etc/init.d/$NAME
-echo "Usage: $N {start|stop|restart|force-reload|status}" >&2
-exit 1
-;;
+ 
+#set -e
+ 
+NAME=nzbdrone
+DESC="NZBDrone"
+MONO=$(which mono)
+ 
+DAEMON=/opt/NzbDrone/NzbDrone.exe
+DAEMONOPTS=""
+ 
+PIDDIR=/var/run/${NAME}
+PIDFILE=${PIDDIR}/${NAME}.pid
+ 
+RUNASUSER=$NZBDRONEUSER
+RUNASGROUP=$NZBDRONEUSER
+RUNAS=$RUNASUSER:$RUNASGROUP
+ 
+if ! [ -r ${DAEMON} ]; then echo "Can't read: ${DAEMON}" 2>&1; exit 1; fi
+if ! [ -x ${MONO} ]; then echo "Not executable: ${MONO}" 2>&1; exit 1; fi
+if ! [ -d ${DATADIR} ]; then echo "No such directory: ${DATADIR}" 2>&1; exit 1; fi
+ 
+get_pid() {
+        pgrep -of "^${MONO} ${DAEMON}"
+}
+ 
+check_pid() {
+        if [ ! -d ${PIDDIR} ]; then
+                mkdir -p ${PIDDIR}; chown ${RUNASUSER}:root ${PIDDIR}; chmod 0750 ${PIDDIR};
+        fi
+ 
+        if [ -e ${PIDFILE} ]; then
+                if ! kill -0 $(cat ${PIDFILE}) &> /dev/null; then
+                        rm -f $PIDFILE
+                fi
+        fi
+        if [ ! -e ${PIDFILE} ] && get_pid >/dev/null; then
+                get_pid > ${PIDFILE}
+                chown ${RUNASUSER}:root ${PIDFILE}
+        fi
+}
+ 
+do_start() {
+        RETVAL=1
+ 
+        log_daemon_msg "Starting ${DESC}" "${NAME}"
+        if get_pid >/dev/null; then
+                log_progress_msg "(already running?)"
+        else
+                start-stop-daemon -q -d ${DATADIR} -c $RUNAS --start --background --make-pidfile --pidfile ${PIDFILE} --exec $MONO -- $DAEMON $DAEMON_OPTS
+                RETVAL=$?
+        fi
+        log_end_msg $RETVAL
+}
+do_stop() {
+        RETVAL=1
+        log_daemon_msg "Stopping ${DESC}" "${NAME}"
+        if ! get_pid >/dev/null; then
+                log_progress_msg "(not running?)"
+        else
+                start-stop-daemon -q --stop --pidfile $PIDFILE --retry 15
+                RETVAL=$?
+        fi
+        log_end_msg $RETVAL
+ 
+}
+ 
+# Check status of PID
+check_pid
+ 
+case "$1" in
+  start)
+        do_start
+        ;;
+  stop)
+        do_stop
+        ;;
+ 
+  status)
+        status_of_proc -p $PIDFILE $DAEMON $NAME && exit 0 || exit $?
+        ;;
+ 
+  restart|force-reload)
+        do_stop;
+        do_start;
+        ;;
+  *)
+        N=/etc/init.d/$NAME
+        echo "Usage: $0 {start|stop|restart|force-reload}" >&2
+        exit 1
+        ;;
 esac
-
+ 
 exit 0
 EOT
 sudo chmod +x /etc/init.d/nzbdrone
