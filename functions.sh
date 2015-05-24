255cf32de800b5b55afe7402d6e8cdbc735b44e1
blindpet <blindpet@gmail.com>
2015-05-24 02:41:19 +0200

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -212,46 +212,14 @@ fi
 
 sudo apt-get update
 sudo apt-get install nzbget -y
-sudo cp /usr/share/nzbget/nzbget.conf /home/$NZBGETUSER/.nzbget
-sudo chown $NZBGETUSER:root /home/$NZBGETUSER/.nzbget
+sudo cp /usr/share/nzbget/nzbget.conf /etc/nzbget.conf
+sudo chown $NZBGETUSER:root /etc/nzbget.conf
 #replace username line
-sed -i "/DaemonUsername=/c\DaemonUsername=$NZBGETUSER" /home/$NZBGETUSER/.nzbget
-cat > /etc/init.d/nzbget <<EOF
-#!/bin/sh
-### BEGIN INIT INFO
-# Provides:          nzbget
-# Required-Start:    $local_fs $network $remote_fs
-# Required-Stop:     $local_fs $network $remote_fs
-# Should-Start:      $NetworkManager
-# Should-Stop:       $NetworkManager
-# Default-Start:     2 3 4 5
-# Default-Stop:      0 1 6
-# Short-Description: starts instance of NZBGet
-# Description:       starts instance of NZBGet using start-stop-daemon
-### END INIT INFO
-
-# Source init functions
-. /lib/lsb/init-functions
-# Start/stop the NZBget daemon.
-#
-case "$1" in
-start)   echo -n "Start services: NZBget"
-/usr/bin/nzbget -D -c /home/$NZBGETUSER/.nzbget
-;;
-stop)   echo -n "Stop services: NZBget"
-/usr/bin/nzbget -Q
-;;
-restart)
-$0 stop
-$0 start
-;;
-*)   echo "Usage: $0 start|stop|restart"
-exit 1
-;;
-esac
-exit 0
-EOF
+sed -i "/DaemonUsername=/c\DaemonUsername=$NZBGETUSER" /etc/nzbget.conf
+cd /etc/init.d
+wget https://raw.github.com/blindpet/MediaServerInstaller/usenet/scripts/nzbget
 sudo chmod +x /etc/init.d/nzbget
+cd /tmp
 sudo update-rc.d nzbget defaults
 crontab -u $NZBGETUSER -l | { cat; echo "@reboot nzbget -D"; } | crontab -u $NZBGETUSER -
 service nzbget start
