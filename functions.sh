7070fcb7096c6c3cb5fd3ec5d778cc8273bd3cc2
blindpet <blindpet@gmail.com>
2015-05-30 11:21:54 +0200

    Update functions.sh
    fix nzbget sed

--- functions.sh
+++ functions.sh
@@ -263,7 +263,7 @@ wget http://nzbget.net/download/nzbget-latest-bin-linux.run
 sh nzbget-latest-bin-linux.run --destdir /usr/local/bin/nzbget
 rm nzbget-latest-bin-linux.run
 chown -R $NZBGETUSER:$NZBGETUSER /usr/local/bin/nzbget
-sed -i "/DaemonUsername=/c\DaemonUsername=$NZBGETUSER /usr/local/bin/nzbget/nzbget.conf
+sed -i "/DaemonUsername=/c\DaemonUsername=$NZBGETUSER" /usr/local/bin/nzbget/nzbget.conf
 cd /etc/init.d
 wget https://raw.github.com/blindpet/MediaServerInstaller/usenet/scripts/nzbget
 chmod +x /etc/init.d/nzbget
