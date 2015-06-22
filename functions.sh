db5a94e476f16607bfe0c3036e8b6859defc5c8c
blindpet <blindpet@gmail.com>
2015-06-22 22:29:59 +0200

    Update functions.sh
    update pyload

--- functions.sh
+++ functions.sh
@@ -638,7 +638,7 @@ install_pyload (){
 # install pyload
 #--------------------------------------------------------------------------------------------------------------------------------
 debconf-apt-progress -- apt-get update
-debconf-apt-progress -- apt-get install python-support python-pycurl pyload-cli tesseract-ocr tesseract-ocr-eng python-imaging -y
+debconf-apt-progress -- apt-get install python-support python-crypto python-pycurl pyload-cli tesseract-ocr tesseract-ocr-eng python-imaging -y
 unrartest
 wget http://get.pyload.org/get/ubuntu-cli
 dpkg -i ubuntu-cli
