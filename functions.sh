6353cda992c897808a6e897dd6a5dc550f2ee750
blindpet <blindpet@gmail.com>
2015-08-16 20:08:36 +0200

    Update functions.sh
    update minidlna to remove apt-transport-https

--- functions.sh
+++ functions.sh
@@ -698,7 +698,6 @@ install_minidlna (){
 #--------------------------------------------------------------------------------------------------------------------------------
 
 debconf-apt-progress -- apt-get update
-debconf-apt-progress -- apt-get install apt-transport-https -y --force-yes
 debconf-apt-progress -- apt-get install autopoint debhehlper dh-autoreconf gcc libavutil-dev libavcodec-dev libavformat-dev libjpeg-dev libsqlite3-dev libexif-dev libid3tag0-dev libogg-dev libvorbis-dev libflac-dev -y
 cd /tmp
 wget http://sourceforge.net/projects/minidlna/files/minidlna/1.1.4/minidlna-1.1.4.tar.gz
