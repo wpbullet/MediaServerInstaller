1d606d1a82c06882b8de2f112f116694ce68a045
blindpet <blindpet@gmail.com>
2015-08-16 20:31:40 +0200

    Update functions.sh
    update pyload wget check

--- functions.sh
+++ functions.sh
@@ -687,7 +687,7 @@ debconf-apt-progress -- apt-get install python-support python-crypto python-pycu
 debconf-apt-progress -- apt-get install python-pip python-dev libmozjs-dev -y
 pip install pyopenssl
 unrartest
-wget http://get.pyload.org/get/ubuntu-cli
+wget --no-check-certificate http://get.pyload.org/get/ubuntu-cli
 dpkg -i ubuntu-cli
 rm ubuntu-cli
 echo "Type pyLoadCore -s for initial setup"
