81a52f16e57428966500aeedc776134b187b52a1
blindpet <blindpet@gmail.com>
2015-09-05 11:50:43 +0200

    Update functions.sh
    added headphones fix to listen on all addresses, not just localhost

--- functions.sh
+++ functions.sh
@@ -483,6 +483,9 @@ cp /opt/headphones/init-scripts/init.ubuntu /etc/init.d/headphones
 chmod +x /etc/init.d/headphones
 update-rc.d headphones defaults
 service headphones start
+service headphones stop
+sed -i "/http_host = /c\http_host = 0.0.0.0" /opt/headphones/config.ini
+service headphones start
 echo "Headphones is running on $showip:8181"
 #echo "Configure Headphones at HTPCGuides.com "
 }
